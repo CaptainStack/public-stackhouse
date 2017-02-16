b765c44b28567afddcd78e95d42180f831b27ccf
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-16 02:54:15 -0800

    Add test of actual code.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 mocha.setup('bdd');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      chai.assert.equal(-1, [1,2,3].indexOf(4));
+describe('corresponding_bracket_index(source_code, caret_index)', function() {
+  describe('valid source code: "[[abc]][({Andre Stackhouse})]0"', function() {
+    it('should return 6 when the caret index is 0', function() {
+      let valid_source_code = '[[abc]][({Andre Stackhouse})]0';
+      chai.assert.equal(6, corresponding_bracket_index(valid_source_code, 0));
     });
   });
 });
