a5cfbe8a4ef7167af68506c928f8e984ce5aa5ac
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-15 16:11:25 -0800

    Create corresponding_bracket_index file + stub.
    Include file in index.html

---
+++ solutions/actual-interviews/corresponding_bracket_index.js
@@ -0,0 +1,5 @@
+// Write a function that takes a string that represents source code and an int that represents a caret index in the source code.
+// If the caret index is pointed at a bracket, curly brace, or parenthesis, return the index of the corresponding brace.
+
+function corresponding_bracket_index(source_code, caret_index) {
+}
