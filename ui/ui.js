7efc2b066235bb89d8edf4d571b10e18e4bc2409
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-17 14:57:56 -0800

    Fix broken pseudoclass selector.

--- ui/ui.js
+++ ui/ui.js
@@ -1,10 +1,14 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-  document.querySelector('select').onchange = () => {
-    let fn = window[$("select option:selected").text()];
+document.addEventListener('DOMContentLoaded', () => {
+  // Watch for a problem being selected from the select input. Look up selected solution and 
+  // display it in the code container.
+  let problem_select = document.querySelector('select');
+  problem_select.addEventListener('change', () => {
+    let fn = window[problem_select[problem_select.selectedIndex].innerText];
     document.querySelector('code').innerText = fn.toString();
     Prism.highlightAll();
-  };
+  });
 
+  // Watch for clicks on the 'run mocha tests' button and run them when clicked.
   document.querySelector('button').onclick = () => {
     mocha.run();
   };
