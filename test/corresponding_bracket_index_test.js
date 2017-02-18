3b0d0be102588a5e9a119758d1c45a474d3079e3
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-18 02:50:00 -0800

    Rename test.

--- test/corresponding_bracket_index_test.js
+++ test/corresponding_bracket_index_test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe('corresponding_bracket_index', function() {
+describe('corresponding_bracket_index_test', function() {
   let valid_source_code     = '[[abc]][({Andre Stackhouse})]0';
   //                           012345678901234567890123456789
   let improper_nesting      = '12([)]444';
