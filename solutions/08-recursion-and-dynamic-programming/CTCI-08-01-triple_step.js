8fd20ef85fe2df79d09331fb6c6127c6c549af35
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-17 19:51:16 -0800

    Add problem formulation to triple_step.js

--- solutions/08-recursion-and-dynamic-programming/CTCI-08-01-triple_step.js
+++ solutions/08-recursion-and-dynamic-programming/CTCI-08-01-triple_step.js
@@ -1,4 +1,7 @@
 function triple_step(n) {
+  // A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a 
+  // time. Implement a method to count how many possible ways the child can run up the stairs.
+  
   let triple_step_helper = (current_step, total_steps) => {
     if (current_step === total_steps) {
       return 1;
