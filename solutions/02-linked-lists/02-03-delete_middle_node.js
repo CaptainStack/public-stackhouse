b77e3025cc2a06c49714cb6ebdd3ceb0360d3403
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-18 03:04:45 -0800

    Implement delete_middle_node.js

---
+++ solutions/02-linked-lists/02-03-delete_middle_node.js
@@ -0,0 +1,22 @@
+function delete_middle_node(list, node) {
+  // Cracking the Coding Interview problem 2.3
+  // Implement an algorithm to delete a node in the middle (i.e., any node but the first and last node,
+  // not necessarily the exact middle) of a singly linked list, given only access to that node.
+  // EXAMPLE
+  // Input: the node c from the linked list a->b->c->d->e->f
+  // Result: nothing is returned by the new linked list looks like a->b->d->e->f
+
+  let previous = list;
+  let current = list.next;
+  let deleted = false;
+
+  while (!deleted && current) {
+    if (node === current) {
+      previous.next = current.next;
+      deleted = true;
+    } else {
+      previous = current;
+      current = current.next;
+    }
+  }
+}
