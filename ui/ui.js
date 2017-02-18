4beb8cb343b716c2339ae510762b4b069dbf7c18
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-17 21:00:00 -0800

    Update test UI.

--- ui/ui.js
+++ ui/ui.js
@@ -2,15 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // Watch for a problem being selected from the select input. Look up selected solution and 
   // display it in the code container.
   let problem_select = document.querySelector('select');
+  let selected_option;
+  let all_tests_button = document.querySelector('#all-tests-button');
   problem_select.addEventListener('change', () => {
-    let fn = window[problem_select[problem_select.selectedIndex].innerText];
+    selected_option = problem_select[problem_select.selectedIndex].innerText;
+    let fn = window[selected_option];
     document.querySelector('code').innerText = fn.toString();
     Prism.highlightAll();
   });
 
   // Watch for clicks on the 'run mocha tests' button and run them when clicked.
-  document.querySelector('button').onclick = () => {
-    mocha.setup('bdd');
+  all_tests_button.onclick = () => {
     mocha.run();
+    all_tests_button.disabled = true;
+    all_tests_button.title = "Refresh page to run Mocha tests again.";
   };
 }, false );
