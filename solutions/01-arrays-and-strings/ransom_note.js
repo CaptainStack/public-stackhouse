57cc1c8310314298dc223709e5b41c137b9ef24a
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-21 11:08:50 -0800

    Fix formatting on ransom_note.

--- solutions/01-arrays-and-strings/ransom_note.js
+++ solutions/01-arrays-and-strings/ransom_note.js
@@ -2,9 +2,9 @@ let note = "I have your daughter";
 let string_repo = "I have your daughter and her birthday present";
 
 function ransom_note(note, string_repo) {
-  // Given a string, a "ransom note," and a second string, a "collection of newspapers/magazines," write 
-  // a function that returns true if the second string contains the correct collection of characters such 
-  // that the ransom note can be recreated and return false otherwise.
+  // Given a string, a "ransom note," and a second string, a "collection of newspapers/magazines," 
+  // write a function that returns true if the second string contains the correct collection of 
+  // characters such that the ransom note can be recreated and return false otherwise.
 
   // Helper to convert a string into a map of characters to number of instances
   let convert_string_to_character_map = (string) => {
@@ -29,9 +29,9 @@ function ransom_note(note, string_repo) {
 
   let note_character_map = convert_string_to_character_map(note);
   
-  // Iterate through the string repo and remove characters from the character map one at a time if they
-  // were in the original ransom note. If the map is successfully emptied, the note can be recreated and
-  // the function returns true.
+  // Iterate through the string repo and remove characters from the character map one at a time if 
+  // they were in the original ransom note. If the map is successfully emptied, the note can be 
+  // recreated and the function returns true.
   for (let character of string_repo) {
     if (map_is_empty(note_character_map)) {
       return true;
