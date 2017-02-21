cab1d6c88ed6e5501f79b5a6d013ad051488299d
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-21 11:05:27 -0800

    Fix formatting on urlify.

--- solutions/01-arrays-and-strings/01-03-urlify.js
+++ solutions/01-arrays-and-strings/01-03-urlify.js
@@ -1,12 +1,10 @@
 let character_array = 'Mr John Smith    '.split('');
 
 function urlify(character_array, actual_length) {
-  // Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient
-  // space at the end to hold the additional characters, and that you are given the "true" length of the string.
-  // Use a character array so that you can perform this operation in place.
-  // EXAMPLE 
-  // input:  "Mr John Smith    ", 13
-  // output: "Mr%20John%20Smith"
+  // Write a method to replace all spaces in a string with '%20'. You may assume that the string has 
+  // sufficient space at the end to hold the additional characters, and that you are given the "true" 
+  // length of the string. Use a character array so that you can perform this operation in place.
+  // EXAMPLE: (input: "Mr John Smith    ", 13) (output: "Mr%20John%20Smith")
 
   let current_string_index = actual_length - 1;
   let swap_index = character_array.length - 1;
