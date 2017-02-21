4f4f20e6559eaa64ad4df6dbdaea32ae81d0a27c
Andre Stackhouse <CaptainStack@outlook.com>
2017-02-21 01:04:49 -0800

    Implement validate_brackets.js

---
+++ solutions/01-arrays-and-strings/validate_brackets.js
@@ -0,0 +1,17 @@
+function validate_brackets(string) {
+  // Given a string that represents source code, write a function that checks if the braces are properly
+  // paired and nested.
+  // E.g. [({aa})] <- valid [(aa]) <- invalid [aa} <- invalid [hello <- invalid
+  
+  let brackets = [];
+  for (character of string) {
+    if (character === "(" || character === "{" || character === "[") {
+      brackets.push(character);
+    } else if (character === ")" && brackets.pop() !== "(" ||
+               character === "}" && brackets.pop() !== "{" ||
+               character === "]" && brackets.pop() !== "[") {
+      return false;
+    }
+  }
+  return brackets.isEmpty();
+}
