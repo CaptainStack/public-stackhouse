037a0324fe03e7f56eb722c0d17b666a0d3def7a
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-05 14:55:22 -0800

    Add problem specification to list_of_depths.

--- solutions/04-trees-and-graphs/04-03-list_of_depths.js
+++ solutions/04-trees-and-graphs/04-03-list_of_depths.js
@@ -1,4 +1,8 @@
 function list_of_depths(tree) {
+  // Cracking the Coding Interview 4.3
+  // Given a binary tree, design an algorithm which creates a linked list of all the nodes 
+  // at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+  
   let lists = [];
   let lists_of_depths_helper = (branch, depth) => {
     if (lists[depth]) {
