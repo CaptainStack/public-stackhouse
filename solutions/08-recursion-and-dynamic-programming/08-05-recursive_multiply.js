71f06d95d7ae6be129e4da5db2508b8898cb61cc
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-08 23:39:41 -0800

    Update to use bit shifting (division not allowed).

--- solutions/08-recursion-and-dynamic-programming/08-05-recursive_multiply.js
+++ solutions/08-recursion-and-dynamic-programming/08-05-recursive_multiply.js
@@ -14,13 +14,13 @@ function recursive_multiply(a, b) {
     } else if (factor_2 % 2 === 0) {
       // If factor_2 is even, calculate the product of half of factor_2 and factor_1 and double it.
       // E.g. 5 * 6 === 5(3 * 2)
-      total += recursive_multiply_helper(factor_2 / 2, total);
+      total += recursive_multiply_helper(factor_2 >> 1, total);
       return total + total;
     } else {
       // If factor_2 is odd, subtract 1 to make it even, do the same halving and doubling as above, and
       // add factor_1 at the end to calculate the final result.
       // E.g. 5 * 7 === 5(3 * 2) + 5
-      total += recursive_multiply_helper((factor_2 - 1) / 2, total);
+      total += recursive_multiply_helper((factor_2 - 1) >> 1, total);
       total += total;
       total += factor_1;
       return total;
