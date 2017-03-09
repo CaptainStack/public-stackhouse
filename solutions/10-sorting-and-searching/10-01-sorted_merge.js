7a6434a902f2fea9476e079350684cf3ef2de2c2
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-09 00:16:22 -0800

    Fix sorted_merge implementation.

--- solutions/10-sorting-and-searching/10-01-sorted_merge.js
+++ solutions/10-sorting-and-searching/10-01-sorted_merge.js
@@ -3,7 +3,7 @@ function sorted_merge(A, B) {
   // You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.
   // Write a method to merge B into A in sorted order.
 
-  let swap_index = A.length -1;
+  let swap_index = A.length - 1;
   let B_index = B.length - 1;
   let A_index = 0;
   for (let [index, value] of A.entries()) {
@@ -14,7 +14,7 @@ function sorted_merge(A, B) {
   }
 
   while (B_index >= 0) {
-    if (B[B_index] >= A[A_index]) {
+    if (B[B_index] >= A[A_index] || !A[A_index]) {
       A[swap_index] = B[B_index];
       B_index--;
       swap_index--;
