c0f3e4e42cac62530ef20bef58e9dfca81f39a21
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-11 15:00:53 -0800

    Add toString method to TreeNode.

--- data-structures/TreeNode.js
+++ data-structures/TreeNode.js
@@ -3,6 +3,26 @@ const TreeNode = (data) => {
   return {
     data: data,
     left: null,
-    right: null
+    right: null,
+    toString: function() {
+      let lines = [];
+      helper = (branch, level) => {
+        if (!lines[level]) lines[level] = '';
+        if (!lines[level + 1]) lines[level + 1] = '';
+        lines[level] += branch.data + ' ';
+        if (branch.left) {
+          lines[level + 1] += '/ '
+          helper(branch.left, level + 2);
+        }
+        if (branch.right) {
+          lines[level + 1] += '\\ ';
+          helper(branch.right, level + 2)
+        }
+      }
+      helper(this, 0);
+      for (line of lines) {
+        console.log(line);
+      }
+    }
   }
 }
